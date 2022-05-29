b5db05e2e725c7def46e19dc2ee9219bd7cef904
WiredDControl <wireddcontrol@gmail.com>
2022-05-29 13:31:27 +0200

    bugfix (minor)

--- games/templatetags/image_previewfile.py
+++ games/templatetags/image_previewfile.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 @register.filter
 def preview(value):
-    previewfilename = (value.rsplit(".")[0]) + "_thumb.jpg"
+    previewfilename = (value.rsplit(".",1)[0]) + "_thumb.jpg"
     return previewfilename
